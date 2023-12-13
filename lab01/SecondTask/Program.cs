3bf13f52580510e1e1860b30d8fd7240e0066df0
Frodelian <olegbodwk@gmail.com>
2023-12-14 02:28:32 +0300

    first commit

---
+++ lab01/SecondTask/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondEx
+{
+    public class Rectangle // Класс Rectangle
+    {
+        public Rectangle() // Два поля описывающие длины сторон 
+        {
+            side1 = 0;
+            side2 = 0;
+        }
+
+        public Rectangle(double sideA, double sideB) // Конструктор инициализирующий поля side1 и side2
+        {
+            side1 = sideA;
+            side2 = sideB;
+        }
+
+        private double AreaCalculator() // Площадь прямоугольника 
+        {
+            return side1 * side2;
+        }
+
+        private double PerimeterCalculator() // Периметр прямоугольника
+        {
+            return 2 * side1 + 2 * side2;
+        }
+
+        // Свойства вызывающие созданные ранее методы
+        public double Area
+        {
+            // get выполняет действия по получению значения свойства
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+
+        public double side1;
+        public double side2;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstSide;
+            double secondSide;
+
+            Console.WriteLine("Введите размер первой стороны: ");
+            double.TryParse(Console.ReadLine(), out firstSide);
+
+            Console.WriteLine("Введите размер второй стороны: ");
+            double.TryParse(Console.ReadLine(), out secondSide);
+
+            Rectangle r1 = new Rectangle(firstSide, secondSide);
+
+            Console.WriteLine("Площадь прямоугольник равна {0}", r1.Area);
+            Console.WriteLine("Периметр прямоугольник равен {0}", r1.Perimeter);
+
+        }
+    }
+}
---
+++ lab03/FirstTask/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirtTask
+{
+    public struct Vector
+    {
+        public double X;
+        public double Y;
+        public double Z;
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public static Vector operator +(Vector vec1, Vector vec2)
+        {
+            return new Vector(vec1.X + vec2.X, vec1.Y + vec2.Y, vec1.Z + vec2.Z);
+        }
+        // maybe dot x * x + y * y + z * z
+        public static Vector operator *(Vector vec1, Vector vec2)
+        {
+            return new Vector(vec1.X * vec2.X, vec1.Y * vec2.Y, vec1.Z * vec2.Z);
+        }
+        public static Vector operator *(Vector vec, double number)
+        {
+            return new Vector(vec.X * number, vec.Y * number, vec.Z * number);
+        }
+        public double Length()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+        }
+        public static bool operator <(Vector vec1, Vector vec2)
+        {
+            return vec1.Length() < vec2.Length();
+        }
+        public static bool operator >(Vector vec1, Vector vec2)
+        {
+            return vec1.Length() > vec2.Length();
+        }
+        public static bool operator ==(Vector vec1, Vector vec2)
+        {
+            return vec1.Length() == vec2.Length();
+        }
+        public static bool operator !=(Vector vec1, Vector vec2)
+        {
+            return vec1.Length() != vec2.Length();
+        }
+        public static bool operator <=(Vector vec1, Vector vec2)
+        {
+            return vec1.Length() <= vec2.Length();
+        }
+        public static bool operator >=(Vector vec1, Vector vec2)
+        {
+            return vec1.Length() >= vec2.Length();
+        }
+
+        public override string ToString()
+        {
+            return X.ToString() + " " + Y.ToString() + " " + Z.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector a = new Vector(11, 22, 33);
+            Vector b = new Vector(44, 55, 66);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a * b);
+            //Console.WriteLine(a * 1.5);
+            Console.WriteLine(a.Length());
+            Console.WriteLine(b.Length());
+            Console.WriteLine(a < b);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(a <= b);
+            Console.WriteLine(a >= b);
+        }
+    }
+}
